ba5690814cd015e8b6a9627266a43c837ae10103
francisco-zarp <francisco.berti@prestadores.intermedica.com.br>
2022-09-05 17:20:36 -0300

    Desenvolvimento dos layouts de listagem e detalhe de partidas

---
+++ app/src/main/java/dio/me/simulator/MainActivity.java
@@ -0,0 +1,22 @@
+package dio.me.simulator;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import dio.me.simulator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+    }
+}
